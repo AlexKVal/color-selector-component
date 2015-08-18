bc477634fbfa4d28c607e76b099b2e28c745bb6e
AlexKVal <alexkval@gmail.com>
2015-08-18 21:54:03 +0300

    Added 'active' option handling

--- src/ColorOption.js
+++ src/ColorOption.js
@@ -1,18 +1,32 @@
 import React, {PropTypes, Component} from 'react'
+import classNames from 'classnames'
 
 export default class ColorOption extends Component {
+  constructor (props, context) {
+    super(props, context)
+
+    this._handleOnClick = this._handleOnClick.bind(this)
+  }
+
+  _handleOnClick () {
+    this.props.onClick(this.props.color)
+  }
+
   render () {
     const {
+      active,
       title,
-      color,
-      ...props
+      color
     } = this.props
+
+    const aClassName = classNames({selected: active}, 'color-btn')
 
     return (
       <li>
         <a href='#'
+          onClick={this._handleOnClick}
           title={title}
-          className='color-btn'
+          className={aClassName}
           style={{backgroundColor: color}} />
       </li>
     )
@@ -20,6 +34,8 @@ export default class ColorOption extends Component {
 }
 
 ColorOption.propTypes = {
+  active: PropTypes.bool,
   color: PropTypes.string,
+  onClick: PropTypes.func,
   title: PropTypes.string
 }
--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,4 +1,4 @@
-import React, {PropTypes, Component} from 'react'
+import React, {PropTypes, Component, cloneElement, Children} from 'react'
 import classNames from 'classnames'
 
 class ColorSelector extends Component {
@@ -6,13 +6,25 @@ class ColorSelector extends Component {
     super(props, context)
 
     this.state = {expanded: props.defaultExpanded}
+
     this._handleDropdownClick = this._handleDropdownClick.bind(this)
+    this._renderOption = this._renderOption.bind(this)
   }
 
   _handleDropdownClick (e) {
     this.setState({expanded: !this.state.expanded})
 
     e.preventDefault()
+  }
+
+  _renderOption (child) {
+    return cloneElement(
+      child,
+      {
+        active: child.props.color === this.state.colorSelected,
+        onClick: (color) => this.setState({ colorSelected: color })
+      }
+    )
   }
 
   render () {
@@ -30,7 +42,7 @@ class ColorSelector extends Component {
         </a>
 
         <ul className='dropdown-menu dropdown-caret'>
-          {this.props.children}
+          {Children.map(this.props.children, this._renderOption)}
         </ul>
       </div>
     )
